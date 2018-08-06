d01d488de8888e7ab3a61ae23107b97b54f1d157
George Peek <gvpeek@gmail.com>
2018-08-06 17:15:30 -0500

    final bug fixes and formatting corrections

--- scripts/joins_fuzzy_matching_etc.py
+++ scripts/joins_fuzzy_matching_etc.py
@@ -44,9 +44,9 @@ def get_rdi(street_address, zip_code):
 
     if not result:
         #print("No candidates. This means the address is not valid.")
-        return('invalid')
+        return 'invalid'
 
-    return(result[0])
+    return result[0]
 
 
 def check_table_exists(table_name):
@@ -63,7 +63,7 @@ dbname = 'map_the_vote'
 username = 'docker'
 password = 'docker'
 
-engine = create_engine('postgres://%s:%s@localhost/%s'%(username,password,dbname))
+engine = create_engine('postgres://%s:%s@localhost/%s' % (username, password, dbname))
 print(engine.url)
 
 if not database_exists(engine.url):
@@ -72,6 +72,7 @@ if not database_exists(engine.url):
 print(database_exists(engine.url))
 
 con = psycopg2.connect(host='localhost', database = dbname, user = username, password=password)
+
 
 def initial_joins():
     print('## Initial Joins')
@@ -114,7 +115,8 @@ def initial_joins():
     sql_query = """
     DROP TABLE IF EXISTS voters_join;
     CREATE TABLE voters_join AS
-    SELECT a.geom, a.oa_lon, a.oa_lat, a.oa_number, a.oa_postcode, a.oa_street_address, a.oa_street, b.* FROM select_all_addresses a LEFT JOIN voter_file_all b ON upper(a.oa_street_address) = b.vf_street_address;
+    SELECT a.geom, a.oa_lon, a.oa_lat, a.oa_number, a.oa_postcode, a.oa_street_address, a.oa_street, b.*
+    FROM select_all_addresses a LEFT JOIN voter_file_all b ON upper(a.oa_street_address) = b.vf_street_address;
     """
     engine.execute(sql_query)
 
@@ -122,7 +124,9 @@ def initial_joins():
     sql_query = """
     DROP TABLE IF EXISTS addresses_not_joined;
     CREATE TABLE addresses_not_joined AS
-    SELECT geom, oa_lon, oa_lat, oa_number, oa_postcode, oa_street_address, oa_street FROM voters_join WHERE vf_voter_file_vanid IS NULL;
+    SELECT geom, oa_lon, oa_lat, oa_number, oa_postcode, oa_street_address, oa_street
+    FROM voters_join
+    WHERE vf_voter_file_vanid IS NULL;
     """
     engine.execute(sql_query)
 
@@ -149,33 +153,36 @@ def perform_fuzzy_matching():
     sql_query = """
     SELECT * FROM addresses_not_joined;
     """
-    addresses_not_joined = pd.read_sql_query(sql_query,con)
+    addresses_not_joined = pd.read_sql_query(sql_query, con)
 
     print('Get voters not joined')
     sql_query = """
     SELECT * FROM voters_not_joined;
     """
-    voters_not_joined = pd.read_sql_query(sql_query,con)
+    voters_not_joined = pd.read_sql_query(sql_query, con)
 
     print('Get unique street addresses from voter records')
-    unique_street_address = voters_not_joined[['vf_streetno','vf_zip5','vf_street_address']].drop_duplicates()
+    unique_street_address = voters_not_joined[['vf_streetno', 'vf_zip5', 'vf_street_address']].drop_duplicates()
 
     unique_street_address['fuzzy_match'] = ''
     unique_street_address['fuzzy_score'] = 0
 
     print('Calculate closest address matches')
-    for i in range(0,unique_street_address.shape[0]):
+    current_time = datetime.now()
+    for i in range(0, unique_street_address.shape[0]):
         street_no = unique_street_address.iloc[i]['vf_streetno']
         zip_code = unique_street_address.iloc[i]['vf_zip5']
         street_address = unique_street_address.iloc[i]['vf_street_address']
         potential_matches = addresses_not_joined.loc[(addresses_not_joined.oa_number == street_no) & (addresses_not_joined.oa_postcode == zip_code)]['oa_street_address'].values
-        if (len(potential_matches)>0):
+        if len(potential_matches) > 0:
             closest_match = difflib.get_close_matches(street_address, potential_matches, n=1)
-            if(len(closest_match)>0):
+            if len(closest_match) > 0:
                 match_score = difflib.SequenceMatcher(None, street_address, closest_match[0]).ratio()
-                unique_street_address.iloc[i,unique_street_address.columns.get_loc('fuzzy_match')] = closest_match
-                unique_street_address.iloc[i,unique_street_address.columns.get_loc('fuzzy_score')] = match_score
-        if((i % 100) == 0):
+                unique_street_address.iloc[i, unique_street_address.columns.get_loc('fuzzy_match')] = closest_match
+                unique_street_address.iloc[i, unique_street_address.columns.get_loc('fuzzy_score')] = match_score
+        if i % 1000 == 0:
+            print('Time for batch: {0}'.format(datetime.now() - current_time))
+            current_time = datetime.now()
             print(i)
 
     table_name = "voter_addresses_fuzzy_match"
@@ -244,21 +251,24 @@ def smarty_streets_rdi_check():
         ON a.oa_street_address = b.oa_street_address
         WHERE b.Index IS NULL;
         """
-        addresses_to_check = pd.read_sql_query(sql_query,con)
+        addresses_to_check = pd.read_sql_query(sql_query, con)
 
     print('{0} addresses to check'.format(len(addresses_to_check)))
 
-    addresses_checked = pd.DataFrame(index=range(0, addresses_to_check.shape[0],1),columns=['oa_street_address','oa_street_address_no_zip','zip_code','residential','vacant','active','ss_lon','ss_lat'],dtype='object')
+    addresses_checked = pd.DataFrame(index=range(0, addresses_to_check.shape[0], 1),
+                                     columns=['oa_street_address', 'oa_street_address_no_zip', 'zip_code',
+                                              'residential', 'vacant', 'active', 'ss_lon', 'ss_lat'],
+                                     dtype='object')
 
     current_time = datetime.now()
-    for i in range(0,addresses_to_check.shape[0],1):
+    for i in range(0, addresses_to_check.shape[0], 1):
         oa_street_address = addresses_to_check.iloc[i].oa_street_address
         address_no_zip = oa_street_address[0:-6]
         zipcode = oa_street_address[-5:]
         addresses_checked.loc[i]['oa_street_address'] = oa_street_address
         addresses_checked.loc[i]['oa_street_address_no_zip'] = address_no_zip
         addresses_checked.loc[i]['zipcode'] = zipcode
-        result = get_rdi(address_no_zip,zipcode)
+        result = get_rdi(address_no_zip, zipcode)
         if result == 'invalid':
             addresses_checked.loc[i]['residential'] = 'invalid'
         else:
@@ -267,7 +277,7 @@ def smarty_streets_rdi_check():
             addresses_checked.loc[i]['active'] = result.analysis.active
             addresses_checked.loc[i]['ss_lon'] = result.metadata.longitude
             addresses_checked.loc[i]['ss_lat'] = result.metadata.latitude
-        if(i%1000 == 0):
+        if i % 1000 == 0:
             print('Time for batch: {0}'.format(datetime.now() - current_time))
             current_time = datetime.now()
             print(i)
@@ -295,9 +305,9 @@ def smarty_streets_geocode_voters():
     if not check_table_exists('smarty_streets_geocode'):
         print('smarty_streets_geocode does not exist, get all')
         sql_query = """
-        SELECT DISTINCT(a.vf_street_address) FROM voters_still_not_joined;
+        SELECT DISTINCT(vf_street_address) FROM voters_still_not_joined;
         """
-        voters_to_geocode = pd.read_sql_query(sql_query,con)
+        voters_to_geocode = pd.read_sql_query(sql_query, con)
     else:
         sql_query = """
         SELECT DISTINCT(a.vf_street_address) FROM voters_still_not_joined a
@@ -305,18 +315,21 @@ def smarty_streets_geocode_voters():
         ON a.vf_street_address = b.vf_street_address
         WHERE b.vf_street_address IS NULL;
         """
-        voters_to_geocode = pd.read_sql_query(sql_query,con)
+        voters_to_geocode = pd.read_sql_query(sql_query, con)
 
-    voters_geocoded = pd.DataFrame(index=range(0, voters_to_geocode.shape[0],1),columns=['vf_street_address','vf_street_address_no_zip','ss_lon','ss_lat','residential','vacant'],dtype='object')
+    voters_geocoded = pd.DataFrame(index=range(0, voters_to_geocode.shape[0], 1),
+                                   columns=['vf_street_address', 'vf_street_address_no_zip', 'ss_lon', 'ss_lat',
+                                            'residential', 'vacant'],
+                                   dtype='object')
 
     current_time = datetime.now()
-    for i in range(0,voters_to_geocode.shape[0],1):
+    for i in range(0, voters_to_geocode.shape[0], 1):
         vf_street_address = voters_to_geocode.iloc[i].vf_street_address
         address_no_zip = vf_street_address[0:-6]
         zipcode = vf_street_address[-5:]
         voters_geocoded.loc[i]['vf_street_address'] = vf_street_address
         voters_geocoded.loc[i]['vf_street_address_no_zip'] = address_no_zip
-        result = get_rdi(address_no_zip,zipcode)
+        result = get_rdi(address_no_zip, zipcode)
         if result == 'invalid':
             voters_geocoded.loc[i]['residential'] = 'invalid'
         else:
@@ -325,11 +338,10 @@ def smarty_streets_geocode_voters():
             voters_geocoded.loc[i]['active'] = result.analysis.active
             voters_geocoded.loc[i]['ss_lon'] = result.metadata.longitude
             voters_geocoded.loc[i]['ss_lat'] = result.metadata.latitude
-        if(i % 1000 == 0):
+        if i % 1000 == 0:
             print('Time for batch: {0}'.format(datetime.now() - current_time))
             current_time = datetime.now()
             print(i)
-
 
     print('Insert new rows into table')
     voters_geocoded.head(CHUNK_SIZE).to_sql("smarty_streets_geocode", engine, if_exists='append')
@@ -338,17 +350,26 @@ def smarty_streets_geocode_voters():
             print(i)
             voters_geocoded[i:(i + CHUNK_SIZE)].to_sql("smarty_streets_geocode", engine, if_exists='append')
 
-    # if table is created new
-    # sql_query = """
-    # SELECT AddGeometryColumn('smarty_streets_geocode','geom',4326,'POINT',2);
-    # """
-    # engine.execute(sql_query)
+    print('Set "geom" column')
+    current_time = datetime.now()
+    try:
+        # TODO will this iterate over all of the records even if they have already been set?
+        geom_update_sql_query = """
+        UPDATE smarty_streets_geocode
+        SET geom = ST_SetSRID(ST_MakePoint(ss_lon::double precision, ss_lat::double precision), 4326);
+        """
+        engine.execute(geom_update_sql_query)
+    except ProgrammingError:
+        # table is created new
+        print('New table. Adding "geom" column')
+        sql_query = """
+        SELECT AddGeometryColumn('smarty_streets_geocode','geom',4326,'POINT',2);
+        """
+        engine.execute(sql_query)
+        print('setting "geom" column')
+        engine.execute(geom_update_sql_query)
 
-    # TODO will this iterate over all of the records even if they have already been set?
-    sql_query = """
-    UPDATE smarty_streets_geocode SET geom = ST_SetSRID(ST_MakePoint(ss_lon::double precision, ss_lat::double precision), 4326);
-    """
-    engine.execute(sql_query)
+    print('Time to set "geom" column: {0}'.format(datetime.now() - current_time))
 
 
 def final_joins_and_processing():
@@ -357,7 +378,8 @@ def final_joins_and_processing():
     print('create table voters_not_joined_geocoded by joining geocoded voters not joined with original table that identified them')
     sql_query = """
     CREATE TABLE voters_not_joined_geocoded AS
-    SELECT a.*, b.geom FROM voters_still_not_joined a LEFT JOIN smarty_streets_geocode b ON a.vf_street_address = b.vf_street_address;
+    SELECT a.*, b.geom
+    FROM voters_still_not_joined a LEFT JOIN smarty_streets_geocode b ON a.vf_street_address = b.vf_street_address;
     """
     engine.execute(sql_query)
 
@@ -367,8 +389,9 @@ def final_joins_and_processing():
     pd.read_sql_query(sql_query,con)
 
     print('now append voters_not_joined_geocoded to voters_join')
-    # I should reconsider doing an insert here, since it means I will need to redo the original join if I want to change anything
-    # maybe I can add a field that tells how the record was geocoded, althought, that should be recorded by whether the row has an oa_address
+    # I should reconsider doing an insert here, since it means I will need to redo the original join if I want to
+    # change anything maybe I can add a field that tells how the record was geocoded, althought, that should be
+    # recorded by whether the row has an oa_address
     sql_query = """
     INSERT INTO voters_join (geom, vf_voter_file_vanid, vf_sex, vf_age, vf_street_address, vf_multi_unit, vf_cntyvtd)
     SELECT geom, vf_voter_file_vanid, vf_sex, vf_age, vf_street_address, vf_multi_unit, vf_cntyvtd
@@ -377,8 +400,9 @@ def final_joins_and_processing():
     engine.execute(sql_query)
 
     print('now append oa_fuzzy_join to voters_join')
-    # I should reconsider doing an insert here, since it means I will need to redo the original join if I want to change anything
-    # maybe I can add a field that tells how the record was geocoded, althought, that should be recorded by whether the row has an oa_address
+    # I should reconsider doing an insert here, since it means I will need to redo the original join if I want to
+    # change anything maybe I can add a field that tells how the record was geocoded, althought, that should be
+    # recorded by whether the row has an oa_address
     sql_query = """
     INSERT INTO voters_join (geom, vf_voter_file_vanid, vf_sex, vf_age, vf_street_address, vf_multi_unit, vf_cntyvtd)
     SELECT geom, vf_voter_file_vanid, vf_sex, vf_age, vf_street_address, vf_multi_unit, vf_cntyvtd
@@ -389,7 +413,7 @@ def final_joins_and_processing():
     sql_query = """
     SELECT COUNT(*) FROM voters_join;
     """
-    print('Total voters joined: {0}'.format(pd.read_sql_query(sql_query,con)))
+    print('Total voters joined: {0}'.format(pd.read_sql_query(sql_query, con)))
 
     print('Create table voters_not_joined_geocoded_3081')
     sql_query = """
@@ -478,12 +502,14 @@ def final_joins_and_processing():
     sql_query = """
     SELECT COUNT(*) FROM addresses_still_not_joined_3081 WHERE join_mask = 1;
     """
-    pd.read_sql_query(sql_query,con)
+    pd.read_sql_query(sql_query, con)
 
     print('Create table final_addresses_not_joined')
     sql_query = """
     CREATE TABLE final_addresses_not_joined AS
-    SELECT a.*, b.residential, b.vacant, b.active FROM addresses_still_not_joined_3081 a LEFT JOIN smarty_streets_rdi_check_new b ON a.oa_street_address = b.oa_street_address;
+    SELECT a.*, b.residential, b.vacant, b.active
+    FROM addresses_still_not_joined_3081 a
+    LEFT JOIN smarty_streets_rdi_check_new b ON a.oa_street_address = b.oa_street_address;
     """
     engine.execute(sql_query)
 
@@ -537,9 +563,9 @@ def final_joins_and_processing():
     """
     print('Total distinct address on voters_join_3081: {0}'.format(pd.read_sql_query(sql_query, con)))
 
-
-initial_joins()
-perform_fuzzy_matching()
-smarty_streets_rdi_check()
-smarty_streets_geocode_voters()
-final_joins_and_processing()
+if __name__ == '__main__':
+    initial_joins()
+    perform_fuzzy_matching()
+    smarty_streets_rdi_check()
+    smarty_streets_geocode_voters()
+    final_joins_and_processing()
